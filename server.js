44505f6d821e090df74aedca3479179dbde7218b
bri <bcamesi@gmail.com>
2022-11-03 17:01:11 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -28,6 +28,16 @@ app.get('/tip/:total/:tipPercentage', (request, response) => {
     Tip Amount: $${tipAmount}`)
 })
 
+
+// magic 8 ball
+
+const magic8Ball = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+app.get('/magic/:question', (request, response) => {
+    const random = magic8Ball[Math.floor(Math.random() * magic8Ball.length)];
+    response.send(`${request.params.question}? <h1>${random}</h1>`)
+})
+
 app.listen(PORT, () => {
     console.log(`listening to post ${PORT}`)
 })
