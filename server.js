1c5f3066d609f6b048f52607f6ddb5a3cd524b93
bri <bcamesi@gmail.com>
2022-11-03 15:18:04 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 
+// greetings
 
 app.get('/greeting', (request, response) => {
     response.send("Hello, stranger")
@@ -15,6 +16,17 @@ app.get('/greeting/:name', (request, response) => {
     response.send(`Wow! Hello there, ${request.params.name}`)
 })
 
+
+// tip calculator
+
+app.get('/tip/:total/:tipPercentage', (request, response) => {
+    const total = `${request.params.total}`;
+    const tipPercentage = `${request.params.tipPercentage}`
+    let tipAmount = total * tipPercentage/100;
+    response.send(`Total: $${total}
+    Tip Percentage: ${tipPercentage}%
+    Tip Amount: $${tipAmount}`)
+})
 
 app.listen(PORT, () => {
     console.log(`listening to post ${PORT}`)
